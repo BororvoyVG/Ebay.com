0c9d948659ebd088d8b7f6402fa4ed0926c3ec4b
Borovoy <ilovemetallist@gmail.com>
2019-09-03 07:22:26 +0300

    AfterTest is added

--- ebaycom/src/test/java/Settings.java
+++ ebaycom/src/test/java/Settings.java
@@ -1,21 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Settings {
-
     WebDriver driver;
-//    @BeforeAll
-//    public void setUpDriver() {
-//        System.setProperty("webdriver.chrome.driver","C:/Repository/Ebay.com/chromedriver.exe");
-//        driver = new ChromeDriver();
-//    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeEach
+    public void setUpDriver() {
+        System.setProperty("webdriver.chrome.driver", "C://Users//ВН/Downloads/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void openHomePage() {
-        System.setProperty("webdriver.chrome.driver", "C://Users//ВН/Downloads/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://epam.com");
+
+        driver.get("http://ebay.com");
+    }
+    @AfterEach
+    public void finishTest() {
+        driver.quit();
     }
 }
